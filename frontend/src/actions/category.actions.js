55b962bcda2c7b2d04c0403770eca7ce5a7af54a
14110145 <waptop4u@gmail.com>
2021-02-27 01:46:32 +0700

    14.2 Show and Add Categories

--- frontend/src/actions/category.actions.js
+++ frontend/src/actions/category.actions.js
@@ -1,8 +1,41 @@
 import axiosIntance from "../helpers/axios";
+import { categoryContants } from "./constants";
 
 export const getAllCategory = () => {
   return async (dispatch) => {
+    dispatch({
+      type: categoryContants.GET_ALL_CATEGORIES_REQUEST,
+    });
     const res = await axiosIntance.get(`/category/getcategory`);
-    console.log(res);
+    if (res.status === 200) {
+      const { categoryList } = res.data;
+      dispatch({
+        type: categoryContants.GET_ALL_CATEGORIES_SUCCESS,
+        payload: { categories: categoryList },
+      });
+    } else {
+      dispatch({
+        type: categoryContants.GET_ALL_CATEGORIES_FAILURE,
+        payload: { error: res.data.error },
+      });
+    }
   };
-}; 
+};
+
+export const addCategory = (form) => {
+  return async (dispatch) => {
+    dispatch({ type: categoryContants.ADD_NEW_CATEGORY_REQUEST });
+    const res = await axiosIntance.post(`/category/create`, form);
+    if (res.status === 200) {
+      dispatch({
+        type: categoryContants.ADD_NEW_CATEGORY_SUCCESS,
+        payload: res.data.category,
+      });
+    }else{
+      dispatch({
+        type: categoryContants.ADD_NEW_CATEGORY_SUCCESS,
+        payload: res.data.error,
+      });
+    }
+  };
+};

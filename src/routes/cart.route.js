e6e1282feebdcd3934de0424a7a8516133aaeb9d
unknown <waptop4u@gmail.com>
2021-04-07 23:31:59 +0700

    35.1 Cart api fix

--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { requireSignin, userMiddleware } = require("../common-middleware");
-const { addItemToCart } = require("../controllers/cart.controller");
+const { addItemToCart, getCartItems } = require("../controllers/cart.controller");
 const router = express.Router();
 
 router.post("/user/cart/addtocart", requireSignin, userMiddleware, addItemToCart);
-router.get("/user/getCartItems", requireSignin, userMiddleware, getCartItems);
+router.post("/user/getCartItems", requireSignin, userMiddleware, getCartItems);
 
 module.exports = router;
